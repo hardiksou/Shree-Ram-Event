19e3c6918be1e33d17126cdc07f5083533b0079e
Chavda Hardik <hardikchavda2324@gmail.com>
2025-07-18 22:56:37 +0530

    fronted conneect backend

--- backend/index.js
+++ backend/index.js
@@ -9,7 +9,13 @@ const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(express.static('frontend'));
+
+// Static client files
+app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+});
 
 app.post('/send', async (req, res) => {
   const { name, email, message } = req.body;
