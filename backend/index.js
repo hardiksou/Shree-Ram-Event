6fae6f7063fd50021e16361081ea3c5cb23135f4
Chavda Hardik <hardikchavda2324@gmail.com>
2025-07-18 23:19:14 +0530

    Update index.js change connect fronted

--- backend/index.js
+++ backend/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
+  res.sendFile(path.join(__dirname, '..', 'frontend', 'index.html'));
 });
 
 app.post('/send', async (req, res) => {
