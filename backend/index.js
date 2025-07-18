4bfbc4e71ef5cbed2ef1fb7bb088dac00e415df6
Chavda Hardik <hardikchavda2324@gmail.com>
2025-07-18 23:02:08 +0530

    changes backend

--- backend/index.js
+++ backend/index.js
@@ -13,7 +13,7 @@ app.use(express.json());
 // Static client files
 app.use(express.static(path.join(__dirname, '../frontend')));
 
-app.get('/', (req, res) => {
+app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../frontend', 'index.html'));
 });
 
